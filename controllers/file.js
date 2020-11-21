83488e074cfd2eb67e7b45a50b7e8182834b1167
Sergio Fabian Ortiz Valencia <sfortiz@bancolombia.com.co>
2020-11-21 01:06:57 -0500

    New function cartesian plane in html

--- controllers/file.js
+++ controllers/file.js
@@ -18,9 +18,46 @@ exports.readDeliveryFile = (req, res) => {
     let deliverys = split.map((delivery, index) => { 
       return { "id": index, "delivery": delivery };
     });
+    makeCartesianPlane(dron);
     res.status(200).send({ "Deliverys": deliverys });
   }
   else{
     res.status(404).send({ "message": "Delivery file NOT FOUND", "internal_code": 404 });
   }
 }
+
+function makeCartesianPlane(dron){
+  var html='';
+  html +=`<html>
+            <body>
+            
+            <canvas id="myCanvas" width="200" height="200"
+            style="border:1px solid #d3d3d3;">
+            Your browser does not support the canvas element.
+            </canvas>
+            
+            <script>
+            
+            var canvas = document.getElementById("myCanvas");
+            var ctx = canvas.getContext("2d");
+            ctx.moveTo(100,0);
+            ctx.lineTo(100,200);
+            ctx.moveTo(0,100);
+            ctx.lineTo(200,100);
+            ctx.fillStyle = "black";
+            ctx.fillRect(67, 67, 6, 6);
+            ctx.font = "14px Arial";
+            ctx.fillText("(-3,3)",60,60);
+            ctx.stroke();
+            
+            </script>
+            
+            </body>
+            </html>`;
+
+            fs.writeFile('./tracking/'+ dron +'.html', html, function (err) {
+              if (err) throw err;
+              console.log('Saved!');
+            });
+}
+    
